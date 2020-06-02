c238bfcf43fdbfa16f8cef368d48a62691e1eae7
rafaelvannes <vannesoey@gmail.com>
2020-06-02 23:50:27 +0700

    number sum revised

--- numbersum.py
+++ numbersum.py
@@ -1,36 +1,25 @@
-numinp = input('Enter a number: ')
-if numinp == 'done':
-    quit()
-try:
-    numtype = float(numinp)
-except ValueError:
-    print ('Invalid input')
-    pass
-
 total = 0.0
 totalcount = 0
-maxlist = []
+numlist = []
 
 while True:
-    numinp = input('Enter a number: ')
-    if str(numinp) == 'done':
-        break
     try:
-        numtype = float(numinp)
+        numinp = input('Enter a number: ')
+        if numinp == 'done':
+            break
+        numinp = float(numinp)
     except ValueError:
         print ('Invalid input')
-        total -= numtype
-        totalcount -= 1
-        pass
-    maxlist.append (numtype)
-    total += numtype
+        continue
+    numlist.append (numinp)
+    total += numinp
     totalcount += 1
 
 print ('Total : ' + str(total))
 print ('Count : ' + str(totalcount))
 print ('Average : ' + str(total/totalcount))
-print ('Maximum : ' + str(max(maxlist)))
-print ('Minimum : ' + str(min(maxlist)))
+print ('Maximum : ' + str(max(numlist)))
+print ('Minimum : ' + str(min(numlist)))
 
 
 
